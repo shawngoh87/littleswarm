7b2b08ea7ea59adfe844e3f9c455afd9e1f7371b
shawngoh87 <shawngoh87@hotmail.com>
2016-11-23 16:06:53 +0800

    costFunction revamp, added real-time plotting.
    New: dist() function to calculate Euclidean distance.
    New: costFunction_stub(), will swap with original costFunction once done.
    New: REAL-TIME PLOTTING !!
    Added several bias constants:
    inertiaFactor -- to increase effect of previous velocity
    costBias -- stub constant, scales the cost output
    costRadius -- the radius of point inclusion for cost calculation

--- PSO_2D.py
+++ PSO_2D.py
@@ -13,11 +13,37 @@ from math import *
 # Namespace manipulation
 rand = random.random()
 
+#----------------------------FUNCTIONS------------------------------#
 def randomVector(range):
     """Return a float
     Float is chosen between the two elements of a input list.
     """
     return random.uniform(range[0], range[1])
+
+
+def dist(pointA, pointB):
+    """Return Euclidean distance between A and B"""
+    return np.linalg.norm(np.array(pointA) - np.array(pointB))
+
+
+def costFunction_stub(costBias, costRadius, currentPosition, pointArray):
+    """Return cost as float
+    Calculate the cost according to [x, y], where f(x, y) = blah blah.
+
+    Idea: Take previous point counts into account so the robots leaves a "trail of shadow"
+
+    Changelog:
+    1. Test: cos(x*y)*((x-0.2)^2-y^2)
+    2. Changed cost to the sum of points within a certain radius costRadius.
+        Added costBias for the lulz.
+    """
+    count = 0
+    for point in pointArray:
+        if dist(currentPosition, point) < costRadius:
+            count+=1
+        else: pass
+
+    cost = costBias*count
 
 
 def costFunction(currentPosition): # currentPosition = [x, y, z, ..., last-dimension]
@@ -33,17 +59,19 @@ def costFunction(currentPosition): # currentPosition = [x, y, z, ..., last-dimen
     return (cos(x*y)*(x**2.0-y**2.0))
 
 
-def updateVelocity(c1, c2, currentVelocity, currentPosition, particleBestPosition, globalBestPosition, maxVelocity):
+def updateVelocity(c1, c2, currentVelocity, currentPosition,
+                   particleBestPosition, globalBestPosition, maxVelocity,
+                   inertiaFactor):
     """Return [new X velocity, new Y velocity]
     Calculates axial velocity according to PSO velocity formula.
 
     Changelog:
     1. Negative velocity are now bound by maxVelocity.
     """
-    xVelocity = currentVelocity[0] + \
+    xVelocity = inertiaFactor*currentVelocity[0] + \
                 (c1*rand*(particleBestPosition[0] - currentPosition[0])) + \
                 (c2*rand*(globalBestPosition[0] - currentPosition[0]))
-    yVelocity = currentVelocity[1] + \
+    yVelocity = inertiaFactor*currentVelocity[1] + \
                 (c1 * rand * (particleBestPosition[1] - currentPosition[1])) + \
                 (c2 * rand * (globalBestPosition[1] - currentPosition[1]))
     if abs(xVelocity) >= maxVelocity:
@@ -68,7 +96,9 @@ def updatePosition(currentPosition, currentVelocity):
     return newPosition
 
 
-def createParticle(c1, c2, maxParticle, searchSpace, globalBestPosition, globalBestCost, maxVelocity):
+def createParticle(c1, c2, maxParticle, searchSpace,
+                   globalBestPosition, globalBestCost, maxVelocity,
+                   inertiaFactor):
     """Return a bunch of initialized lists/values.
     Creates the following:
     particleCurrentPosition -- Every particle's current position in [x, y]
@@ -95,7 +125,8 @@ def createParticle(c1, c2, maxParticle, searchSpace, globalBestPosition, globalB
 
     for i in range(maxParticle):
         particleVelocity.append(updateVelocity(c1, c2, initialVelocity, particleCurrentPosition[i],
-                                               particleBestPosition[i], globalBestPosition, maxVelocity))
+                                               particleBestPosition[i], globalBestPosition, maxVelocity,
+                                               inertiaFactor))
     return particleCurrentCost, particleBestCost, particleVelocity, \
            particleCurrentPosition, particleBestPosition, globalBestPosition, globalBestCost
 
@@ -144,16 +175,17 @@ def updatePersonalBest(particleCurrentPosition, particleCurrentCost, particleBes
 problemSize = 0 # Not used yet
 searchSpace = [[-500.0,500.0], [-500.0, 500.0]] # [[x_range], [y_range]]
 initialVelocity = [0.0,0.0]
-maxIteration = 100
+maxIteration = 1000
 maxVelocity = 20
-maxParticle = 40
+maxParticle = 5
 populationSize = 50 # Not used yet
 iteration = 0
 stopCondition = 1 # Not used yet
 mode = 'min' # optimize for max/min
 
-c1 = 4
-c2 = 4
+c1 = 2 # personal best bias
+c2 = 2 # global best bias
+inertiaFactor = 1 # 0.5 < x < 2.0
 globalBestPosition = [0,0]
 globalBestCost = 1000000.0
 
@@ -167,7 +199,9 @@ debugAllCost = []
 # Particle creation
 particleCurrentCost, particleBestCost, particleVelocity, \
 particleCurrentPosition, particleBestPosition, globalBestPosition, globalBestCost \
-= createParticle(c1, c2, maxParticle, searchSpace, globalBestPosition, globalBestCost, maxVelocity)
+= createParticle(c1, c2, maxParticle, searchSpace,
+                 globalBestPosition, globalBestCost,
+                 maxVelocity, inertiaFactor)
 
 # Initialize plot
 plt.figure()
@@ -175,40 +209,40 @@ plt.xlim(searchSpace[0][0],searchSpace[0][1])
 plt.ylim(searchSpace[1][0],searchSpace[1][1])
 
 # Iterate to maxIteration or stopCondition = False
-while(iteration < maxIteration and stopCondition): # TODO: stopCondition as a function of cohesion
+while(iteration < maxIteration and stopCondition): # TODO: stopCondition as a function of cohesion/area coverage
 
     # Iterate through all particles
     for n in range(maxParticle):
         print("iteration " + str(iteration) + " particle " + str(n))
-        particleVelocity[n] = updateVelocity(c1, c2, particleVelocity[n], particleCurrentPosition[n], \
-                                             particleBestPosition[n], globalBestPosition, maxVelocity)
+        particleVelocity[n] = updateVelocity(c1, c2, particleVelocity[n], particleCurrentPosition[n],
+                                             particleBestPosition[n], globalBestPosition, maxVelocity,
+                                             inertiaFactor)
         particleCurrentPosition[n] = updatePosition(particleCurrentPosition[n], particleVelocity[n])
         particleCurrentCost[n] = costFunction(particleCurrentPosition[n])
         particleBestPosition[n], particleBestCost[n] = updatePersonalBest(particleCurrentPosition[n], \
                                                                     particleCurrentCost[n], particleBestPosition[n], particleBestCost[n])
-        debugAllPosition.append([])
-        debugAllCost.append([])
-        debugAllPosition[iteration].append(particleCurrentPosition[n])
-        debugAllCost[iteration].append(particleCurrentCost[n])
+        # debugAllPosition.append([])
+        # debugAllCost.append([])
+        # debugAllPosition[iteration].append(particleCurrentPosition[n])
+        # debugAllCost[iteration].append(particleCurrentCost[n])
+        plt.plot(particleCurrentPosition[n][0], particleCurrentPosition[n][1], 'b.')
+        plt.pause(0.05)
 
     globalBestPosition, globalBestCost = updateGlobalBest(mode, particleBestPosition, particleBestCost, globalBestPosition, globalBestCost)
-    debugBestPosition.append(globalBestPosition)
-    debugBestCost.append(globalBestCost)
+    # debugBestPosition.append(globalBestPosition)
+    # debugBestCost.append(globalBestCost)
 
     iteration+=1
 
-
 # Plot
-# iteration = 1
-for i in range(iteration):
-    for j in range(maxParticle):
-        plt.plot(debugAllPosition[i][j][0], debugAllPosition[i][j][1], 'b.')
-    plt.plot(debugBestPosition[i][0], debugBestPosition[i][1], 'rp')
-    # plt.savefig()
+# for i in range(iteration):
+#     for j in range(maxParticle):
+#         plt.plot(debugAllPosition[i][j][0], debugAllPosition[i][j][1], 'b.')
+#     plt.plot(debugBestPosition[i][0], debugBestPosition[i][1], 'rp')
+#
+# plt.show()
 
-plt.show()
-
-# DEBUG PRINT
+# PRINT
 # print(particleCurrentPosition[:2])
 # print(particleCurrentCost[:2])
 # print(particleBestPosition[:2])
